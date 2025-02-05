f0df7db5c95073fc958742038145960d2abef388
bentran1vn <tan182205@gmail.com>
2025-02-05 16:19:42 +0700

    Fix InCorrect Model

--- BEAUTIFY_AUTHORIZATION.DOMAIN/Entities/LivestreamRoom.cs
+++ BEAUTIFY_AUTHORIZATION.DOMAIN/Entities/LivestreamRoom.cs
@@ -1,20 +1,21 @@
-﻿namespace BEAUTIFY_AUTHORIZATION.DOMAIN.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BEAUTIFY_AUTHORIZATION.DOMAIN.Entities;
 public class LivestreamRoom : AggregateRoot<Guid>, IAuditableEntity
 {
-    public string? Name { get; set; }
-    public string? Description { get; set; }
+    [MaxLength(100)] public required string Name { get; set; }
     public TimeOnly? StartDate { get; set; }
     public TimeOnly? EndDate { get; set; }
-    public string? Status { get; set; }
+    [MaxLength(50)] public string? Status { get; set; }
     public DateOnly? Date { get; set; }
-    public string? Type { get; set; }
+    [MaxLength(50)] public string? Type { get; set; }
     public int? Duration { get; set; }
     public int? TotalViewers { get; set; }
     public Guid? ClinicId { get; set; }
-    public virtual Clinic Clinic { get; set; } = null!;
+    public virtual Clinics Clinic { get; set; }
 
 
     public DateTimeOffset CreatedOnUtc { get; set; }
     public DateTimeOffset? ModifiedOnUtc { get; set; }
-    public virtual ICollection<Promotion>? Promotions { get; set; } = [];
+    public virtual ICollection<Promotion>? Promotions { get; set; }
 }
--- BEAUTIFY_AUTHORIZATION.PERSISTENCE/ApplicationDbContext.cs
+++ BEAUTIFY_AUTHORIZATION.PERSISTENCE/ApplicationDbContext.cs
@@ -12,29 +12,4 @@ public class ApplicationDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder builder) =>
         builder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
-
-    public DbSet<Category> Categories { get; set; }
-    public DbSet<ClinicOnBoardingRequest> ClinicOnBoardingRequests { get; set; }
-    public DbSet<Clinic> Clinics { get; set; }
-    public DbSet<ClinicVoucher> ClinicVouchers { get; set; }
-    public DbSet<Config> Configs { get; set; }
-    public DbSet<Conversation> Conversations { get; set; }
-    public DbSet<CustomerSchedule> CustomerSchedules { get; set; }
-    public DbSet<DoctorCertificate> DoctorCertificates { get; set; }
-    public DbSet<DoctorService> DoctorServices { get; set; }
-    public DbSet<Feedback> Feedbacks { get; set; }
-    public DbSet<LivestreamRoom> LivestreamRooms { get; set; }
-    public DbSet<Message> Messages { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<OrderDetail> OrderDetails { get; set; }
-    public DbSet<Procedure> Procedures { get; set; }
-    public DbSet<Promotion> Promotions { get; set; }
-    public DbSet<Role> Roles { get; set; }
-    public DbSet<Service> Services { get; set; }
-    public DbSet<SubscriptionPackage> SubscriptionPackages { get; set; }
-    public DbSet<SystemTransaction> SystemTransactions { get; set; }
-    public DbSet<User> Users { get; set; }
-    public DbSet<UserClinic> UserClinics { get; set; }
-    public DbSet<Voucher> Vouchers { get; set; }
-    public DbSet<WorkingSchedule> WorkingSchedules { get; set; }
 }
