b9e6a2d2684058450e891219c83fca27aeaaea81
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-03-05 01:43:26 +0700

    Refactor password validation rules in RegisterCommandValidators for simplification

--- BEAUTIFY_AUTHORIZATION.CONTRACT/Services/Identity/Validators/RegisterCommandValidators.cs
+++ BEAUTIFY_AUTHORIZATION.CONTRACT/Services/Identity/Validators/RegisterCommandValidators.cs
@@ -1,7 +1,6 @@
 using FluentValidation;
 
 namespace BEAUTIFY_AUTHORIZATION.CONTRACT.Services.Identity.Validators;
-
 public class RegisterCommandValidators : AbstractValidator<Command.RegisterCommand>
 {
     public RegisterCommandValidators()
@@ -12,11 +11,11 @@ public class RegisterCommandValidators : AbstractValidator<Command.RegisterComma
 
         RuleFor(x => x.Password)
             .NotEmpty()
-            .MinimumLength(8).WithMessage("Password must be at least 8 characters long")
-            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
-            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
-            .Matches(@"\d").WithMessage("Password must contain at least one digit")
-            .Matches(@"[\W_]").WithMessage("Password must contain at least one special character");
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long");
+        //.Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+        // .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
+        //  .Matches(@"\d").WithMessage("Password must contain at least one digit")
+        //  .Matches(@"[\W_]").WithMessage("Password must contain at least one special character");
 
         RuleFor(x => x.FirstName)
             .NotEmpty()
@@ -38,10 +37,10 @@ public class RegisterCommandValidators : AbstractValidator<Command.RegisterComma
             .NotEmpty()
             .Must(BeAtLeast18YearsOld).WithMessage("User must be at least 18 years old");
 
-        RuleFor(x => x.Address)
+      /*  RuleFor(x => x.Address)
             .NotEmpty()
             .MinimumLength(10).WithMessage("Address must be at least 10 characters long")
-            .MaximumLength(100).WithMessage("Address must exceed 100 characters");
+            .MaximumLength(100).WithMessage("Address must exceed 100 characters");*/
     }
 
     private bool BeAtLeast18YearsOld(DateOnly dateOfBirth)
