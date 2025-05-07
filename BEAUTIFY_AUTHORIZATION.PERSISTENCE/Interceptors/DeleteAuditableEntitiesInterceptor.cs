9645e5f52e866a1671540ac57d8ec51d4bc56c70
bentran1vn <tan182205@gmail.com>
2025-05-08 01:06:01 +0700

    Add Interceptor Authorization

---
+++ BEAUTIFY_AUTHORIZATION.PERSISTENCE/Interceptors/DeleteAuditableEntitiesInterceptor.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace BEAUTIFY_AUTHORIZATION.PERSISTENCE.Interceptors;
+public sealed class DeleteAuditableEntitiesInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+        var dbContext = eventData.Context;
+
+        if (dbContext is null)
+            return base.SavingChangesAsync(
+                eventData,
+                result,
+                cancellationToken);
+
+        var entries =
+            dbContext
+                .ChangeTracker
+                .Entries();
+
+        var vietnamTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+        foreach (var entityEntry in entries)
+            if (entityEntry.State == EntityState.Deleted)
+            {
+                var entity = entityEntry.Entity;
+                var isDeletedProperty = entity.GetType().GetProperty("IsDeleted");
+                var updateProperty = entity.GetType().GetProperty("ModifiedOnUtc");
+                if (isDeletedProperty != null && isDeletedProperty.CanWrite && updateProperty != null &&
+                    updateProperty.CanWrite)
+                {
+                    entityEntry.State = EntityState.Modified;
+                    isDeletedProperty.SetValue(entity, true);
+                    updateProperty.SetValue(entity,
+                        TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, vietnamTimeZone));
+                }
+            }
+
+        return base.SavingChangesAsync(
+            eventData,
+            result,
+            cancellationToken);
+    }
+}
---
+++ BEAUTIFY_AUTHORIZATION.PERSISTENCE/Interceptors/UpdateAuditableEntitiesInterceptor.cs
@@ -0,0 +1,45 @@
+﻿using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Abstractions.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace BEAUTIFY_AUTHORIZATION.PERSISTENCE.Interceptors;
+public sealed class UpdateAuditableEntitiesInterceptor
+    : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+        var dbContext = eventData.Context;
+
+        if (dbContext is null)
+            return base.SavingChangesAsync(
+                eventData,
+                result,
+                cancellationToken);
+
+        var entries =
+            dbContext
+                .ChangeTracker
+                .Entries<IAuditableEntity>();
+
+        var vietnamTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+
+        foreach (var entityEntry in entries)
+        {
+            if (entityEntry.State == EntityState.Added)
+                entityEntry.Property(a => a.CreatedOnUtc).CurrentValue
+                    = TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, vietnamTimeZone);
+
+            if (entityEntry.State == EntityState.Modified)
+                entityEntry.Property(a => a.ModifiedOnUtc).CurrentValue
+                    = TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, vietnamTimeZone);
+        }
+
+        return base.SavingChangesAsync(
+            eventData,
+            result,
+            cancellationToken);
+    }
+}
